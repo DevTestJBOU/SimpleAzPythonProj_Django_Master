126367eca509d2791b8d2b2f5ebc2c88eaf7dc05
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-11-29 12:52:25 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,6 +5,10 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
+          # Bit above duplicated to test SonarCloud 
+           # tested already where did not pick up duplicated sections 
+           # so trying duplicated lines 
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "

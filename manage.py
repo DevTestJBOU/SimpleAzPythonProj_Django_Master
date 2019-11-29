bf13d3803d2cf44ee951eddb83239cdefb15522f
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-11-29 12:47:05 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -10,18 +10,10 @@ if __name__ == '__main__':
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-#
-# bit below I've copied from above to test if the analyser picks up duplicaton
-#
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
+           # Bit above duplicated to test SonarCloud 
+           # tested already where did not pick up duplicated sections 
+           # so trying duplicated lines 
+
         ) from exc
-    execute_from_command_line(sys.argv)
+

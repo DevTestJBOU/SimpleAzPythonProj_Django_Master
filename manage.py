f485b7e3d326e842a2a438186ec239ed69d7b792
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-11-29 12:42:50 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -11,4 +11,17 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+#
+# bit below I've copied from above to test if the analyser picks up duplicaton
+#
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)

06cc3184945e687117b29b490e775bd63415b4c4
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-12-02 11:47:34 +0000

    Update test_site.py

--- test_site.py
+++ test_site.py
@@ -1,23 +1,23 @@
 # ORIGINAL
+from django.test import TestCase
+
+class HelloWorldTest(TestCase):
+    def test_site(self):
+        self.assertEqual(1, 1)
+        
 #from django.test import TestCase
 
 #class HelloWorldTest(TestCase):
 #    def test_site(self):
+#    def test_site(self):
 #        self.assertEqual(1, 1)
-        
-from django.test import TestCase
-
-class HelloWorldTest(TestCase):
-    def test_site(self):
-    def test_site(self):
-        self.assertEqual(1, 1)
-        self.assertEqual(1, 1)
+#        self.assertEqual(1, 1)
 
 
-from django.test import TestCase
+#from django.test import TestCase
 
-class HelloWorldTest(TestCase):
-    def test_site(self):
-    def test_site(self):
-        self.assertEqual(1, 1)
-        self.assertEqual(1, 1)
+#class HelloWorldTest(TestCase):
+#    def test_site(self):
+#    def test_site(self):
+#        self.assertEqual(1, 1)
+#        self.assertEqual(1, 1)

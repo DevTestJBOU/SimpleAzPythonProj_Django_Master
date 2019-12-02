a8f3c2d4d38dcf2f5c0c2b6c081ed575413a03f5
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-12-02 11:58:15 +0000

    Update test_site.py

--- test_site.py
+++ test_site.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 class HelloWorldTest(TestCase):
     def test_site(self):
         self.assertEqual(1, 1)
+               self.assertEqual(1, 1)
         
 #from django.test import TestCase
 

eb7b1506175fea30c2fc3e1a09b209b55c060533
DevTestJBOU <46719520+DevTestJBOU@users.noreply.github.com>
2019-12-02 14:25:41 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 import os
 import sys
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
-    try:
-        from django.core.management import execute_from_command_line
-        from django.core.management import execute_from_command_line
+i __name__ == '__main__':
+i    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld.settings')
+i    try:
+i        from django.core.management import execute_from_command_line
+i        from django.core.management import execute_from_command_line
           # Bit above duplicated to test SonarCloud 
            # tested already where did not pick up duplicated sections 
            # so trying duplicated lines 
-    except ImportError as exc:
-        raise ImportError(
+i    except ImportError as exc:
+i        raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
@@ -19,5 +19,5 @@ if __name__ == '__main__':
            # tested already where did not pick up duplicated sections 
            # so trying duplicated lines 
 
-        ) from exc
+ i       ) from exc
 
